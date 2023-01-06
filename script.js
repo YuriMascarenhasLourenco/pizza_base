c1bd60a932c2c5be8c193fd5b69459b51a848bdb
YuriMascarenhasLourenco <90795230+YuriMascarenhasLourenco@users.noreply.github.com>
2023-01-05 22:31:35 -0300

    feet: cart and mobile cart
    os novos recursos permitem que possa abrir o carrinho e adicionar produtos nele, bem como fecha-lo diminuindo para 0 o numero de pedidos, além do botão de fechar no ambiente mobile

--- script.js
+++ script.js
@@ -19,18 +19,17 @@ pizzaJson.map((item,index)=>{
         c('.pizzaInfo--qt').innerHTML= count
         let key= e.target.closest('.pizza-item').getAttribute('data-key')
         modalKey=key
+        
         c('.pizzaWindowArea').style.opacity= 0;
         c('.pizzaWindowArea').style.display= 'flex'
         setTimeout(()=>{
             c('.pizzaWindowArea').style.opacity=1;
         },200)
-let price
-       
        
         c('.pizzaWindowArea img').src=pizzaJson[key].img;
         c('.pizzaWindowArea h1').innerHTML = pizzaJson[key].name;
         c('.pizzaWindowArea .pizzaInfo--desc').innerHTML=pizzaJson[key].description;
-        c('.pizzaWindowArea .pizzaInfo--actualPrice').innerHTML= `${pizzaJson[key].price[0].toFixed(2)}`;
+        c('.pizzaWindowArea .pizzaInfo--actualPrice').innerHTML= `R$ ${pizzaJson[key].price[2].toFixed(2)}`;
        
 
 
@@ -64,27 +63,100 @@ c('.pizzaInfo--qtmenos').addEventListener('click',function(){
     }  
     c('.pizzaInfo--qt').innerHTML= count;
   })
-cs('.pizzaInfo--size').forEach((item)=>{
+cs('.pizzaInfo--size').forEach((item,itemIndex)=>{
     item.addEventListener('click',()=>{
     c('.pizzaInfo--size.selected').classList.remove('selected');
     item.classList.add('selected')
-    c('.pizzaInfo--actualPrice').innerHTML=pizzaJson[key].price
+    c('.pizzaInfo--actualPrice').innerHTML=`R$ ${pizzaJson[modalKey].price[itemIndex].toFixed(2)}`
 })
   })
  c('.pizzaInfo--addButton').addEventListener('click',()=>{
     let size=c('.pizzaInfo--size.selected').getAttribute('data-key')
-    let identifier = pizzaJson[modalKey]+'@'+size
-    let index = cart.findIndex((item)=>item.identifier = identifier)
+    let identifier = pizzaJson[modalKey].id+'@'+size
+    let index = cart.findIndex((item)=>item.identifier == identifier)
     if(index > -1){
         cart[index].qt+=count
     }else{
         cart.push({
-            pizza:modalKey,
+            id:pizzaJson[modalKey].id,
             qt: count,
             size: size , 
             identifier:identifier
             })
     }
     console.log(cart)
+    updateCart()
     exitModal()
  })
+    c('.menu-openner').addEventListener('click',()=>{
+        if(cart.length>0){
+            c('aside').classList.add('show')
+            c('aside').style.left='0'
+        }else{
+            c('aside').classList.remove('show')
+        }
+    })
+    c('.menu-closer').addEventListener('click',()=>{
+        c('aside').classList.remove('show')
+        c('aside').style.left='100vw'
+    })
+ function updateCart(){
+    c('.menu-openner span').innerHTML=cart.length
+    if(cart.length>0){
+        
+            c('aside').classList.add('show')
+        
+       
+        c('.cart').innerHTML=''
+        let subTotal=0;
+        let desconto;
+        let total;
+        for(let i in cart){
+             let pizzaItem = pizzaJson.find((item)=> item.id == cart[i].id)
+            let cartItem= c('.models .cart--item').cloneNode(true)
+            let pizzaSizeName;
+            
+           switch(cart[i].size){
+            case 0:
+                pizzaSizeName ="P"
+                console.log(pizzaSizeName)
+                break;
+            case 1: 
+            pizzaSizeName ="M"
+                break;
+            case 2:  
+            pizzaSizeName="G";
+                break;
+           }
+           let pizzaName = `${pizzaItem.name} (${pizzaSizeName})`
+            cartItem.querySelector('img').src=pizzaItem.img;
+            cartItem.querySelector('.cart--item--qt').innerHTML= cart[i].qt
+            cartItem.querySelector('.cart--item-nome').innerHTML= pizzaName
+            cartItem.querySelector('.cart--item-qtmais').addEventListener('click',()=>{
+                cart[i].qt++
+                updateCart()
+            })
+            cartItem.querySelector('.cart--item-qtmenos').addEventListener('click',()=>{
+                if(cart[i].qt>1){
+                    cart[i].qt--;
+                }else{
+                    cart.splice(i,1)
+                    
+                    console.log(cart)
+                }
+                updateCart()
+            })
+            subTotal+= cart[i].qt*pizzaItem.price[cart[i].size]
+            
+          c('.cart').appendChild(cartItem)
+
+        }
+        desconto = subTotal*0.1
+        total = subTotal - desconto
+        c('.subtotal span:last-child').innerHTML=`R$ ${subTotal.toFixed(2)}`
+        c('.desconto span:last-child').innerHTML=`R$ ${desconto.toFixed(2)}`;
+        c('.total span:last-child').innerHTML=`R$ ${total.toFixed(2)}`
+    }else{
+        c('aside').classList.remove('show')
+    }
+ }
